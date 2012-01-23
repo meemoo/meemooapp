22f1a421b95a4d1fdba0fc24c31f899bcdfcbaa7
Forrest Oliphant <github.com@sembiki.com>
2012-01-23 14:50:57 +0200

    cat uglify

---
+++ build.sh
@@ -0,0 +1,2 @@
+cat lib/underscore-min.js lib/backbone-min.js js/iframework.js js/iframework-graph.js js/iframework-graph-view.js js/iframework-node.js js/iframework-node-view.js js/iframework-module.js js/iframework-module-view.js js/iframework-module-library.js js/iframework-port.js js/iframework-port-view.js js/iframework-edge.js js/iframework-edge-view.js js/iframework-examples.js | uglifyjs -o iframework-min.js
+
