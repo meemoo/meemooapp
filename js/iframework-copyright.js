fef35817f2658492a2cbb0bd1063a32bfceee713
Forrest Oliphant <github.com@sembiki.com>
2012-01-25 17:00:31 +0200

    copyright and remin

---
+++ js/iframework-copyright.js
@@ -0,0 +1,36 @@
+/*
+
+Meemoo web media wiring
+  by Forrest Oliphant
+    at Sembiki Interactive http://sembiki.com/
+    and Media Lab Helsinki http://mlab.taik.fi/
+    with Mozilla WebFWD http://webfwd.org/
+
+Copyright (c) 2012, Forrest Oliphant
+
+
+Built with backbone.js, jQuery, and jQueryUI
+  http://documentcloud.github.com/backbone/
+  http://jquery.com/
+  http://jqueryui.com/
+
+
+This file is part of Meemoo.
+  
+  Meemoo is free software: you can redistribute it and/or modify 
+  it under the terms of the GNU Affero General Public License as 
+  published by the Free Software Foundation, either version 3 of 
+  the License, or (at your option) any later version.
+  
+  Meemoo is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+  GNU Affero General Public License for more details.
+  
+  You should have received a copy of the GNU Affero General 
+  Public License along with Meemoo. If not, see 
+  <http://www.gnu.org/licenses/>.
+  
+*/
+
+"";
