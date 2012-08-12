aefb4c00ea3211fb43c4be59166d71e290fc3eba
Forrest Oliphant <github.com@sembiki.com>
2012-08-12 16:57:37 +0300

    changed internal canvas ref so that image preview can be turned off, not much change in speed noticed with a few

--- src/nodes/image.js
+++ src/nodes/image.js
@@ -3,17 +3,25 @@
 $(function(){
 
   var template = 
-    '<canvas id="canvas-<%= id %>" class="canvas" width="500" height="500" style="max-width:100%;" />'+
+    // '<canvas id="canvas-<%= id %>" class="canvas" width="500" height="500" style="max-width:100%;" />'+
     '<div class="info" />';
+    // '<input type="checkbox" checked="checked" class="showpreview" id="showpreview-<%= id %>" />'+
+    // '<label for="showpreview-<%= id %>">show preview</label>';
 
   Iframework.NativeNodes["image"] = Iframework.NodeBoxNativeView.extend({
 
     template: _.template(template),
     canvas: null,
     context: null,
+    // events: {
+    //   "click .showpreview": "togglePreview"
+    // },
     initializeCategory: function() {
-      this.canvas = this.$(".canvas")[0];
+      this.canvas = document.createElement("canvas");
+      this.canvas.width = 500;
+      this.canvas.height = 500;
       this.context = this.canvas.getContext('2d');
+      this.showCanvas();
     },
     scale: function(){
       // canvas is shown at this scaling factor
@@ -24,7 +32,23 @@ $(function(){
       image: {
         type: "image"
       }
+    },
+    showCanvas: function(){
+      $(this.canvas).attr({
+        "class": "canvas",
+        "id": "canvas-"+this.model.id,
+        "style": "max-width:100%"
+      });      
+      this.$el.prepend(this.canvas);
     }
+    //,
+    // togglePreview: function(e){
+    //   if (e.target.checked) {
+    //     this.$el.prepend(this.canvas);
+    //   } else {
+    //     this.$("canvas").remove();
+    //   }
+    // }
 
   });
 
