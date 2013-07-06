80b6187f11c80fe4226b3d9fcba9756819075d95
Forrest Oliphant <forrest@sembiki.com>
2013-07-07 00:09:39 +0300

    load local as subgraph #15

--- src/local-app-view.js
+++ src/local-app-view.js
@@ -1,17 +1,19 @@
 $(function(){
 
   var template = 
-    '<a class="url" href="#local/<%= graph.info.url %>"></a>'+
-    '<div class="info">'+
-      '<h2 class="title"><%= graph.info.title %></h2>' +
-      '<p class="description"><%= graph.info.description %></p>' +
-    '</div>';
+    '<a class="url" href="#local/<%= graph.info.url %>"></a> - '+
+    '<a class="macro" title="load as subgraph into current graph" href="#">sub</a> ';
+    // '<div class="info">'+
+    //   '<h2 class="title"><%= graph.info.title %></h2>' +
+    //   '<p class="description"><%= graph.info.description %></p>' +
+    // '</div>';
 
   Iframework.LocalAppView = Backbone.View.extend({
     tagName: "div",
     className: "localapp",
     template: _.template(template),
     events: {
+      "click .macro": "loadAsMacro"
     },
     initialize: function () {
       this.render();
@@ -24,12 +26,25 @@ $(function(){
     },
     render: function () {
       this.$el.html(this.template(this.model.toJSON()));
-      this.$(".url").text(decodeURIComponent(this.model.get("graph")["info"]["url"]));
-      this.$(".info").hide();
+      this.$(".url").text( decodeURIComponent(this.model.get("graph")["info"]["url"]) );
+      // this.$(".info").hide();
     },
     update: function () {
       this.render();
       Iframework.updateCurrentInfo();
+    },
+    loadAsMacro: function (event) {
+      event.preventDefault();
+
+      Iframework.shownGraph.addNode({
+        src: "meemoo:subgraph/subgraph",
+        state: {
+          label: decodeURIComponent(this.model.get("graph")["info"]["url"]),
+          graph: this.model.get("graph")
+        }
+      });
+
+      return false;
     },
     remove: function () {
       this.$el.remove();
