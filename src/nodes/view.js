638a505881e6e688a917970951102955fea608a5
Forrest Oliphant <forrest@forresto.com>
2024-12-23 01:26:46 +0200

    Build

--- src/nodes/view.js
+++ src/nodes/view.js
@@ -1,32 +1,29 @@
 // extends src/node-box-native-view.js
 
-$(function(){
-
-  var template = 
+$(function () {
+  var template =
     // '<canvas id="canvas-<%= id %>" class="canvas" width="500" height="500" style="max-width:100%;" />'+
     '<div class="info" />';
 
-  Iframework.NativeNodes["view"] = Iframework.NodeBoxNativeView.extend({
-
+  Iframework.NativeNodes['view'] = Iframework.NodeBoxNativeView.extend({
     template: _.template(template),
-    initializeCategory: function() {
+    initializeCategory: function () {
       // Add popout button to box
       var self = this;
-      this.model.view.$("button.remove")
-        .after(
-          $('<button type="button" class="popout">popout</button>')
-            .button({ icons: { primary: "icon-popup" }, text: false })
-            .click(function(){
-              self.popout();
-            })
-        );
+      this.model.view.$('button.remove').after(
+        $('<button type="button" class="popout">popout</button>')
+          .button({icons: {primary: 'icon-popup'}, text: false})
+          .click(function () {
+            self.popout();
+          })
+      );
     },
     outputs: {
       image: {
-        type: "image"
-      }
+        type: 'image',
+      },
     },
-    popout: function() {
+    popout: function () {
       if (this.w) {
         // Toggle
         this.popin();
@@ -34,23 +31,24 @@ $(function(){
       }
 
       // Open new window to about:blank
-      this.w = window.open("", "meemooRemoteWindow", "menubar=no,location=no,resizable=yes,scrollbars=no,status=no");
+      this.w = window.open(
+        '',
+        'meemooRemoteWindow',
+        'menubar=no,location=no,resizable=yes,scrollbars=no,status=no'
+      );
       var self = this;
-      this.w.addEventListener("unload", function(){
+      this.w.addEventListener('unload', function () {
         self.popin();
       });
 
       return false;
     },
-    popin: function() {
+    popin: function () {
       if (this.w) {
         this.w = null;
       }
-      
+
       return false;
-    }
-
+    },
   });
-
-
 });
